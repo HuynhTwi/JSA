26139e2bdc9109001f00e07a8406be02ddd4e02a
HuynhTwi <128216361+HuynhTwi@users.noreply.github.com>
2023-03-18 15:43:22 +0700

    src

---
+++ test.js
@@ -0,0 +1,25 @@
+// var a=2
+// var a=5
+// console.log(a)
+// const b=4
+// console.log(b)
+const h1Query= document.getElementById('h1')
+
+const h1onmouseover = () => {
+    h1Query.style = 'color: red; font-size: 50px;'
+}
+const h1onmouseout = () => {
+    h1Query.style = 'color: black; font-size: 100%;'
+}
+const dbclick = () => {
+    alert('db')
+}
+const click = () => {
+    alert('one')
+}
+
+const inputquery= document.querySelector('#')
+
+const handleonkeyup = () => {
+
+}
